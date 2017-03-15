ddf6d553af60d4d3c81720e86890c1ec0b6d704b
Marius Kotlarz <marius.kotlarz@gmail.com>
2017-03-15 17:35:22 +0100

    Change how the user is saved in the session, user -> userId

--- src/main/java/com/onkibot/backend/api/SessionController.java
+++ src/main/java/com/onkibot/backend/api/SessionController.java
@@ -1,6 +1,8 @@
 package com.onkibot.backend.api;
 
+import com.onkibot.backend.database.entities.User;
 import com.onkibot.backend.database.repositories.UserRepository;
+import com.onkibot.backend.exceptions.UserNotFoundException;
 import com.onkibot.backend.models.CredentialsModel;
 import com.onkibot.backend.models.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
@@ -29,14 +30,15 @@ public class SessionController {
         Authentication authentication = new UsernamePasswordAuthenticationToken(credentials.getEmail(), credentials.getPassword());
         SecurityContextHolder.getContext().setAuthentication(authenticationManager.authenticate(authentication));
         UserModel userModel = new UserModel(userRepository.findByEmail(credentials.getEmail()).get());
-        session.setAttribute("user", userModel);
+        session.setAttribute("userId", userModel.getUserId());
         return userModel;
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public UserModel session(HttpSession session) {
-        SecurityContextHolder.getContext().getAuthentication();
-        return (UserModel) session.getAttribute("user");
+        int userId = (int) session.getAttribute("userId");
+        User user = userRepository.findByUserId().orElseThrow(() -> new UserNotFoundException(userId));
+        return new UserModel(user);
     }
 
     @RequestMapping(method = RequestMethod.DELETE)
