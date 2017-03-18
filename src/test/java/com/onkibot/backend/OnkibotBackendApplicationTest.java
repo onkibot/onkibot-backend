c99918755326937e79ed638655dd9b7033e4a524
Marius Kotlarz <marius.kotlarz@gmail.com>
2017-03-18 01:35:37 +0100

    Truncate the tables before running any db related tests

--- src/test/java/com/onkibot/backend/OnkibotBackendApplicationTest.java
+++ src/test/java/com/onkibot/backend/OnkibotBackendApplicationTest.java
@@ -1,10 +1,10 @@
 package com.onkibot.backend;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -12,6 +12,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @SpringApplicationConfiguration(OnkibotBackendApplication.class)
 @WebAppConfiguration
 @EnableJpaRepositories
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:./beforeTestRun.sql")
 public class OnkibotBackendApplicationTest {
     @Test
     public void contextLoads() {
