9272c16a87044213038362313d58dc83c09da35d
Marius Kotlarz <marius.kotlarz@gmail.com>
2017-03-25 14:23:19 +0100

    Correctly reference attending table for users. Also add additional model for user details

--- src/main/java/com/onkibot/backend/api/UserController.java
+++ src/main/java/com/onkibot/backend/api/UserController.java
@@ -4,6 +4,7 @@ import com.onkibot.backend.OnkibotBackendApplication;
 import com.onkibot.backend.database.entities.User;
 import com.onkibot.backend.database.repositories.UserRepository;
 import com.onkibot.backend.exceptions.UserNotFoundException;
+import com.onkibot.backend.models.UserDetailModel;
 import com.onkibot.backend.models.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +16,9 @@ public class UserController {
     private UserRepository userRepository;
 
     @RequestMapping(method = RequestMethod.GET, value = "/{userId}")
-    public UserModel get(@PathVariable int userId) {
+    public UserDetailModel get(@PathVariable int userId) {
         User user = assertUser(userId);
-        return new UserModel(user);
+        return new UserDetailModel(user);
     }
 
     private User assertUser(int userId) {
