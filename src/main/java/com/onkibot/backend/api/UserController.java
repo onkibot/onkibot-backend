4dd95fbccb85dbb37ffa96c1b905822641763ebe
Haakoth <haakoth@stud.ntnu.no>
2017-04-05 15:10:42 +0200

    Added getAll to UserController

--- src/main/java/com/onkibot/backend/api/UserController.java
+++ src/main/java/com/onkibot/backend/api/UserController.java
@@ -5,6 +5,8 @@ import com.onkibot.backend.database.entities.User;
 import com.onkibot.backend.database.repositories.UserRepository;
 import com.onkibot.backend.exceptions.UserNotFoundException;
 import com.onkibot.backend.models.UserModel;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,13 @@ public class UserController {
   public UserModel get(@PathVariable int userId) {
     User user = assertUser(userId);
     return new UserModel(user);
+  }
+
+  @RequestMapping(method = RequestMethod.GET)
+  public List<UserModel> getAll() {
+    ArrayList<UserModel> models = new ArrayList<>();
+    userRepository.findAll().forEach(course -> models.add(new UserModel(course)));
+    return models;
   }
 
   private User assertUser(int userId) {
